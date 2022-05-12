b3ec98b766adcd56ad40dc502a8f147fefb027c2
Arya <aryamishra.in@gmail.com>
2022-05-12 20:53:22 +0100

    updated enum

--- src/main/java/com/ar/autotrade/strategy/swingstock/SwingStockStatus.java
+++ src/main/java/com/ar/autotrade/strategy/swingstock/SwingStockStatus.java
@@ -1,25 +1,10 @@
 package com.ar.autotrade.strategy.swingstock;
 
 public enum SwingStockStatus {
-    PREDICTED("predicted"),
-    BUY_PLACED("buy_placed"),
-    GTT_PLACED("gtt_placed"),
-    EXPIRED("expired"),
-    COMPLETE("complete"),
-    STOPLOSS("stoploss");
-
-    private final String label;
-
-    SwingStockStatus(String label) {
-        this.label = label;
-    }
-
-    public static SwingStockStatus fromString(String text) {
-        for (SwingStockStatus it : SwingStockStatus.values()) {
-            if (it.label.equalsIgnoreCase(text)) {
-                return it;
-            }
-        }
-        return null;
-    }
+    PREDICTED,
+    BUY_PLACED,
+    GTT_PLACED,
+    EXPIRED,
+    COMPLETE,
+    STOPLOSS;
 }
